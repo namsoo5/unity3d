1c1aefb688debfa0f849c35eac9ed6413c7e4c93
namsoo <are34@naver.com>
2018-11-23 11:25:08 +0900

    add animation

---
+++ Final/Assets/Script/CharacterStatus.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterStatus : MonoBehaviour {
+	//*********공격창에서 사용한다.
+	//체력.
+	public int HP = 100;
+	public int MaxHP = 100;
+
+	//공격력
+	public int Power = 10;
+
+	//마지막에 공격한 대상
+	public GameObject lastAttackTarget = null;
+
+	//********GUI및네트워크창에서 사용한다.
+	//플레이어 이름
+	public string characterName = "Player";
+
+	//*******애니메이션 창에서 사용한다.
+	//상태
+	public bool attacking = false;
+	public bool died = false;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Final/Assets/Script/InputManager.cs
+++ Final/Assets/Script/InputManager.cs
@@ -16,18 +16,18 @@ public class InputManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		//슬라이드 시작 지점.
-		if (Input.GetButtonDown ("Fire1"))
-			sliderStartPosition = GetCursorPosition ();
+		if (Input.GetButtonDown ("Fire1"))  //Fire1은 unity에정의되어있는태그-입력을다양하게할수있음
+			sliderStartPosition = GetCursorPosition ();  //현재 포지션위치가져옴
 
 		//화면 너비의 10% 이상 커서를 이동시키면 슬라이드시작으로판단한다.
-		if (Input.GetButton ("Fire1")) {
+		if (Input.GetButton ("Fire1")) {   //누르고있으면
 			if (Vector2.Distance (sliderStartPosition, GetCursorPosition ()) >= (Screen.width * 0.1f))
 				moved = true;
 		}
 
 		//슬라이드가 끝났는가
 		if(!Input.GetButtonUp("Fire1") && !Input.GetButton("Fire1"))
-			moved = false; //슬라이드는끝났다.
+			moved = false; //슬라이드는끝났다. (드래그끝)
 
 			//이동량을구한다.
 		if (moved)
